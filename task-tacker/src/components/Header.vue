8cb258d15c40bcb6e13726ce4661aae683b1c083
Shakib448 <shakiba448@gmail.com>
2021-02-25 09:02:15 +0600

    WIP on Vue Js

---
+++ task-tacker/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style lang="scss">
+header {
+  display: flex;
+  justify-content: content;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
