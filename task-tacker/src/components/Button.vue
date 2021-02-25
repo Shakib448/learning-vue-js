977502ba8c9996e3078fbd8bc6b855360b1938ea
Shakib448 <shakiba448@gmail.com>
2021-02-25 09:09:41 +0600

    WIP on Vue Js

--- task-tacker/src/components/Button.vue
+++ task-tacker/src/components/Button.vue
@@ -1,11 +1,20 @@
 <template>
-  <button class="btn">
-    Add Task
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
   </button>
 </template>
 
 <script>
 export default {
   name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      console.log("click");
+    },
+  },
 };
 </script>
